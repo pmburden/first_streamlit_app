4e86b212f8a9897a226ae21216a0ff603a46b2c5
Paul Burden <83753046+pmburden@users.noreply.github.com>
2022-08-03 16:11:08 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('breakfast menu')
+streamlit.text('01')
+streamlit.text('02')
+streamlit.text('03')
+streamlit.text('04')
+
