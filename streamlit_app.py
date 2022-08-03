8a3e388e3f992379c726ec913d6212e46420f7ad
Paul Burden <83753046+pmburden@users.noreply.github.com>
2022-08-03 16:12:10 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit
 
-streamlit.title('My parents new healthy diner')
+streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('breakfast menu')
-streamlit.text('01')
-streamlit.text('02')
-streamlit.text('03')
-streamlit.text('04')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
